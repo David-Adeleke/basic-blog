372a9d48b73ed1a044808487e46bc315adb2bdf7
David Adeleke <90906886+David-Adeleke@users.noreply.github.com>
2026-05-29 03:00:03 +0100

    Rename animations.css to animation.css

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import { createApp } from 'vue'
 import router from './router/index.js'
 import App from './App.vue'
 import './assets/styles/main.css'
-import './assets/styles/animations.css'
+import './assets/styles/animation.css'
 
 const app = createApp(App)
 app.use(router)
